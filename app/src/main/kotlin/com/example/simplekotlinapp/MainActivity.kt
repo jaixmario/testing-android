76e29e46d749bce1def8dc13d80a63664b82fb20
jaideep <135174306+jaixmario@users.noreply.github.com>
2025-05-01 14:16:39 +0530

    🚀

---
+++ app/src/main/kotlin/com/example/simplekotlinapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.simplekotlinapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this)
+        textView.text = "Hello from Kotlin!"
+        setContentView(textView)
+    }
+}
